c02c8c574d9f289bc88dedb92325c7ba2305667e
Jon <jonathan.r.cooler@gmail.com>
2024-03-09 20:52:39 -0500

    import issues

--- app/lib/hooks.ts
+++ app/lib/hooks.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import type { sectionName } from "/lib/types";
+import type { SectionName } from "@/lib/types";
 
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
