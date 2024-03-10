503f1b8882488b38fe05bc92c8cb15554b330acb
Jon <jonathan.r.cooler@gmail.com>
2024-03-09 20:35:59 -0500

    more import issues

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { links } from "../lib/data";
 import clsx from "clsx";
-import { useActiveSectionContext } from '/context/active-section-context';
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 
 export default function Header() {
